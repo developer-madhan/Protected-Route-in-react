d6ccd8f790fa86ac915bd4400fa2bf37faba9219
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-04-05 21:50:49 +0530

    create basic shape of app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
 
-function App() {
+
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
 
-export default App;
+
+
+const User = () => {
+  return (
+    <div>User</div>
+  )
+}
+
+const Admin = () => {
+  return (
+    <div>Admin</div>
+  )
+}
+const NoPage = () => {
+  return (
+    <div>404 this page not here</div>
+  )
+}
+
+
+
+const App = () => {
+  return (
+    <div className='App' >
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/user' element={<User />} ></Route>
+        <Route path='/admin' element={<Admin />} ></Route>
+        <Route path='*' element={<NoPage />} ></Route>
+      </Routes>
+    </div>
+  )
+}
+
+
+export default App
