09a2e2db91bed28979459f47f9490913ca536cb8
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-04-05 23:11:42 +0530

    successfully created user base router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,188 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Container } from "react-bootstrap";
 
+const USER_TYPES = {
+  PUBLIC: "public_user",
+  REG_USER: "reg_user",
+  ADMIN: "admin_user",
+};
+
+const CURRENT_USER_TYPE = USER_TYPES.ADMIN;
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-
-
+    <div>
+      <Container>
+        <h1>Home</h1>
+      </Container>
+    </div>
+  );
+};
+const Login = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Login</h1>
+      </Container>
+    </div>
+  );
+};
 
 const User = () => {
   return (
-    <div>User</div>
-  )
-}
+    <div>
+      <Container>
+        <h1>User</h1>
+      </Container>
+    </div>
+  );
+};
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div>
+      <Container>
+        <h1>Admin </h1>
+      </Container>
+    </div>
+  );
+};
+const UserProfile = () => {
+  return (
+    <div>
+      <Container>
+        <h1>UserProfile </h1>
+      </Container>
+    </div>
+  );
+};
 const NoPage = () => {
   return (
-    <div>404 this page not here</div>
-  )
-}
+    <div>
+      <Container>
+        <h1>404 this page not here </h1>
+      </Container>
+    </div>
+  );
+};
 
+const PublicElement = ({ children }) => {
+  return <>{children}</>;
+};
 
+const UserElement = ({ children }) => {
+  if (
+    CURRENT_USER_TYPE === USER_TYPES.REG_USER ||
+    CURRENT_USER_TYPE === USER_TYPES.ADMIN
+  ) {
+    return <>{children}</>;
+  } else {
+    return (
+      <div>
+        <Container>
+          <h1>You do not have access to this page!</h1>
+        </Container>
+      </div>
+    );
+  }
+};
+
+const AdminElement = ({ children }) => {
+  if (CURRENT_USER_TYPE === USER_TYPES.ADMIN) {
+    return <>{children}</>;
+  } else {
+    return (
+      <div>
+        <Container>
+          <h1>You do not have access to this page!</h1>
+        </Container>
+      </div>
+    );
+  }
+};
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="d-flex justify-content-around my-5">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/login"}>Login</Link>
+        {CURRENT_USER_TYPE === USER_TYPES.REG_USER ||
+        CURRENT_USER_TYPE === USER_TYPES.ADMIN ? (
+          <>
+            <Link to={"/user"}>User</Link>
+            <Link to={"/userprofile"}>UserProfile</Link>
+          </>
+        ) : null}
+        {CURRENT_USER_TYPE === USER_TYPES.ADMIN ? (
+          <>
+            <Link to={"/admin"}>Admin</Link>
+          </>
+        ) : null}
+      </nav>
+    </>
+  );
+};
+
+const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicElement>
+              <Home />
+            </PublicElement>
+          }
+        ></Route>
+         <Route
+          path="/login"
+          element={
+            <PublicElement>
+              <Login />
+            </PublicElement>
+          }
+        ></Route>
+        <Route
+          path="/user"
+          element={
+            <UserElement>
+              <User />
+            </UserElement>
+          }
+        ></Route>
+        <Route
+          path="/userprofile"
+          element={
+            <UserElement>
+              <UserProfile />
+            </UserElement>
+          }
+        ></Route>
+        <Route
+          path="/admin"
+          element={
+            <AdminElement>
+              <Admin />
+            </AdminElement>
+          }
+        ></Route>
+        <Route path="*" element={<NoPage />}></Route>
+      </Routes>
+    </>
+  );
+};
 
 const App = () => {
   return (
-    <div className='App' >
-      <Routes>
-        <Route path='/' element={<Home />} ></Route>
-        <Route path='/user' element={<User />} ></Route>
-        <Route path='/admin' element={<Admin />} ></Route>
-        <Route path='*' element={<NoPage />} ></Route>
-      </Routes>
+    <div className="App">
+      <Navbar />
+      <AppRouter />
     </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
